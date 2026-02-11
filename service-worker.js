b159bdd200b64c7d1139784145369b44da8bc2a2
Abach <domoyrich@gmail.com>
2026-02-11 23:01:44 +0500

    fix: force margin-left with !important, remove conflicting sidebar/app-content rules
    - app-content margin-left: 310px !important (desktop)
    - 1024px: margin-left: 100px !important (matches sidebar 80px)
    - Remove conflicting sidebar hide in style.css (let app-design handle it)
    - section max-width: 100% instead of 1200px (was centering content)
    - SW v25

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 
 // Bump this value whenever you change static assets so clients don't get stuck
 // on stale cached CSS/JS.
-const CACHE_NAME = 'medical-orientation-v24';
+const CACHE_NAME = 'medical-orientation-v25';
 const urlsToCache = [
     '/',
     '/index.html',

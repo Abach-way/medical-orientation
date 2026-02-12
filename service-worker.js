e14161afa0029e782bdee7d964a575978abe4661
Abach <domoyrich@gmail.com>
2026-02-12 09:48:59 +0500

    fix: profile grid responsive - single column on mobile (v35)

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 
 // Bump this value whenever you change static assets so clients don't get stuck
 // on stale cached CSS/JS.
-const CACHE_NAME = 'medical-orientation-v34';
+const CACHE_NAME = 'medical-orientation-v35';
 const urlsToCache = [
     '/',
     '/index.html',
